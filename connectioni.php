7dd31e1d4f24f63b3c1d0bfa9b6aadf30f53efa9
Suhadmendis <suhad.a.mendis@gmail.com>
2020-07-02 13:04:53 +0530

    up

--- connectioni.php
+++ connectioni.php
@@ -2,15 +2,15 @@
 
 
 
-$GLOBALS['hostname'] = 'localhost';
-$GLOBALS['username'] = 'root';
-$GLOBALS['password'] = '';
-$GLOBALS['db'] = 'bookshop';
+// $GLOBALS['hostname'] = 'localhost';
+// $GLOBALS['username'] = 'root';
+// $GLOBALS['password'] = '';
+// $GLOBALS['db'] = 'bookshop';
 
-// $GLOBALS['hostname'] = '162.252.83.203';
-// $GLOBALS['username'] = 'quotrolk';
-// $GLOBALS['password'] = ':8s7DLa5Wd3B!u';
-// $GLOBALS['db'] = 'quotrolk_bookshop';
+$GLOBALS['hostname'] = '162.252.83.203';
+$GLOBALS['username'] = 'quotrolk';
+$GLOBALS['password'] = ':8s7DLa5Wd3B!u';
+$GLOBALS['db'] = 'quotrolk_bookshop';
  
 $GLOBALS['dbinv'] = mysqli_connect($GLOBALS['hostname'],$GLOBALS['username'],$GLOBALS['password'],$GLOBALS['db']);
  
