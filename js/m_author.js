f44f1a1de0d3e639e930416d6d51904280bf5ddf
Prabodha Jayawardena <pmrjayawardena@gmail.com>
2020-07-14 13:37:52 +0530

    updated author validation check

--- js/m_author.js
+++ js/m_author.js
@@ -1,54 +1,48 @@
 var vue = new Vue({
-  el: '#app',
+  el: "#app",
   data: {
-    en_name: '',
-    REF: ''    
+    en_name: "",
+    REF: "",
   },
-    mounted () {
-        axios
-          .get('m_author_data.php?Command=generate')
-          .then(response => {
-            this.en_name = response.data[1]
-            this.REF = response.data[0]
-        })
-    }
+  mounted() {
+    axios.get("m_author_data.php?Command=generate").then((response) => {
+      this.en_name = response.data[1];
+      this.REF = response.data[0];
+    });
+  },
 });
 
 function GetXmlHttpObject() {
-    var xmlHttp = null;
+  var xmlHttp = null;
+  try {
+    // Firefox, Opera 8.0+, Safari
+    xmlHttp = new XMLHttpRequest();
+  } catch (e) {
+    // Internet Explorer
     try {
-        // Firefox, Opera 8.0+, Safari
-        xmlHttp = new XMLHttpRequest();
+      xmlHttp = new ActiveXObject("Msxml2.XMLHTTP");
     } catch (e) {
-// Internet Explorer
-        try {
-            xmlHttp = new ActiveXObject("Msxml2.XMLHTTP");
-        } catch (e) {
-            xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
-        }
+      xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
     }
-    return xmlHttp;
+  }
+  return xmlHttp;
 }
 
 function keyset(key, e) {
-
-    if (e.keyCode == 13) {
-        document.getElementById(key).focus();
-    }
+  if (e.keyCode == 13) {
+    document.getElementById(key).focus();
+  }
 }
 
 function got_focus(key) {
-    document.getElementById(key).style.backgroundColor = "#000066";
+  document.getElementById(key).style.backgroundColor = "#000066";
 }
 
 function lost_focus(key) {
-    document.getElementById(key).style.backgroundColor = "#000000";
+  document.getElementById(key).style.backgroundColor = "#000000";
 }
 
-
-
 // function getdt() {
-    
 
 //     xmlHttp = GetXmlHttpObject();
 //     if (xmlHttp == null) {
@@ -80,99 +74,79 @@ function lost_focus(key) {
 //     }
 // }
 
+function save_info() {
+  xmlHttp = GetXmlHttpObject();
+  if (xmlHttp == null) {
+    alert("Browser does not support HTTP Request");
+    return;
+  }
+  if (document.getElementById("REF").value == "") {
+    document.getElementById("REF").innerHTML =
+      "<div class='alert alert-warning' role='alert'><span class='center-block'></span></div>";
+    return false;
+  }
 
+  var url = "m_author_data.php";
+  url = url + "?Command=" + "save_item";
+  url = url + "&REF=" + document.getElementById("REF").value;
+  url = url + "&name=" + document.getElementById("name").value;
 
-
-
-function save_info()
-{
-
-    xmlHttp = GetXmlHttpObject();
-    if (xmlHttp == null)
-    {
-        alert("Browser does not support HTTP Request");
-        return;
-    }
-     if (document.getElementById('REF').value == "") {
-        document.getElementById('REF').innerHTML = "<div class='alert alert-warning' role='alert'><span class='center-block'></span></div>";
-        return false;
-    }
-
-    var url = "m_author_data.php";
-    url = url + "?Command=" + "save_item";
-    url = url + "&REF=" + document.getElementById("REF").value;
-    url = url + "&name=" + document.getElementById("name").value;
-    
-    xmlHttp.onreadystatechange = salessaveresult;
-    xmlHttp.open("GET", url, true);
-    xmlHttp.send(null);
-
+  xmlHttp.onreadystatechange = salessaveresult;
+  xmlHttp.open("GET", url, true);
+  xmlHttp.send(null);
 }
-
 
 function salessaveresult() {
-    var XMLAddress1;
+  var XMLAddress1;
 
-    if (xmlHttp.readyState == 4 || xmlHttp.readyState == "complete") {
-
-        if (xmlHttp.responseText == "Saved") {
-            alert(xmlHttp.responseText);
-            // document.getElementById('msg_box').innerHTML = "<div class='alert alert-success' role='alert'><span class='center-block'>Saved</span></div>";
-            // $("#msg_box").hide().slideDown(400).delay(2000);
-            // $("#msg_box").slideUp(400);
-        } else {
-            alert(xmlHttp.responseText);
-            // document.getElementById('msg_box').innerHTML = "<div class='alert alert-warning' role='alert'><span class='center-block'>" + xmlHttp.responseText + "</span></div>";
-        }
+  if (xmlHttp.readyState == 4 || xmlHttp.readyState == "complete") {
+    if (xmlHttp.responseText == "Saved!!!") {
+      alert(xmlHttp.responseText);
+      // document.getElementById('msg_box').innerHTML = "<div class='alert alert-success' role='alert'><span class='center-block'>Saved</span></div>";
+      // $("#msg_box").hide().slideDown(400).delay(2000);
+      // $("#msg_box").slideUp(400);
+    } else {
+      alert(xmlHttp.responseText);
+      // document.getElementById('msg_box').innerHTML = "<div class='alert alert-warning' role='alert'><span class='center-block'>" + xmlHttp.responseText + "</span></div>";
     }
+  }
 }
 
+function getForm(REF, IDF) {
+  xmlHttp = GetXmlHttpObject();
+  if (xmlHttp == null) {
+    alert("Browser does not support HTTP Request");
+    return;
+  }
+  var url = "m_author_data.php";
+  url = url + "?Command=" + "getForm";
+  url = url + "&REF=" + REF;
+  url = url + "&IDF=" + IDF;
 
-function getForm(REF, IDF)
-{
-   
-    xmlHttp = GetXmlHttpObject();
-    if (xmlHttp == null)
-    {
-        alert("Browser does not support HTTP Request");
-        return;
-    }
-    var url = "m_author_data.php";
-    url = url + "?Command=" + "getForm";
-    url = url + "&REF=" + REF;
-    url = url + "&IDF=" + IDF;
-
-    xmlHttp.onreadystatechange = getFromValues;
-    xmlHttp.open("GET", url, true);
-    xmlHttp.send(null);
+  xmlHttp.onreadystatechange = getFromValues;
+  xmlHttp.open("GET", url, true);
+  xmlHttp.send(null);
 }
 
-function getFromValues()
-{
-    var XMLAddress1;
+function getFromValues() {
+  var XMLAddress1;
 
-    if (xmlHttp.readyState == 4 || xmlHttp.readyState == "complete")
-    {
+  if (xmlHttp.readyState == 4 || xmlHttp.readyState == "complete") {
+    XMLAddress1 = xmlHttp.responseXML.getElementsByTagName("IDF");
+    var IDF = XMLAddress1[0].childNodes[0].nodeValue;
 
-        XMLAddress1 = xmlHttp.responseXML.getElementsByTagName("IDF");
-        var IDF = XMLAddress1[0].childNodes[0].nodeValue;
+    XMLAddress1 = xmlHttp.responseXML.getElementsByTagName("objSup");
+    var objSup = JSON.parse(XMLAddress1[0].childNodes[0].nodeValue);
 
-        XMLAddress1 = xmlHttp.responseXML.getElementsByTagName("objSup");
-        var objSup = JSON.parse(XMLAddress1[0].childNodes[0].nodeValue);
-
-        if (IDF === "Master") {
-            opener.document.getElementById('REF').value = objSup.REF;
-            opener.document.getElementById('name').value = objSup.name;
-            
-        }
-        if (IDF === "item") {
-            opener.document.getElementById('author_ref').value = objSup.REF;
-            opener.document.getElementById('author_name').value = objSup.name;
-        }
-
-      
-        self.close();
-    
+    if (IDF === "Master") {
+      opener.document.getElementById("REF").value = objSup.REF;
+      opener.document.getElementById("name").value = objSup.name;
     }
-    
+    if (IDF === "item") {
+      opener.document.getElementById("author_ref").value = objSup.REF;
+      opener.document.getElementById("author_name").value = objSup.name;
+    }
+
+    self.close();
+  }
 }
